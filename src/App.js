c07686c2b9d09ebe9a5168854e4431a50d71a2e8
hectorrene <campa.hector@yahoo.com>
2025-02-27 16:15:06 -0800

    porfis jala

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import fondo from "./assets/fondo.mp4";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <video src={fondo} autoPlay loop muted />
+      <div className="content">
+        <h1> WikiVillanos </h1>
+        <p> conoce a nuestros villlanos</p>
+      </div>
+    </div>
+  );
+}
+
+export default Main; 
