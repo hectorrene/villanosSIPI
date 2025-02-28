eeeccee9ca298659d70bfd0c9a705d5cd1a2e368
hectorrene <campa.hector@yahoo.com>
2025-02-28 00:08:11 -0800

    ninguna otra modificación más que el merge de pruebasGibran

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><a class="nav-link active" href='index.html'><p> Villanos </p></a></li>
+                <li><a href='historia.html'><p> Marvel </p></a></li>
+                <li><a href='conocenos.html'><p> DC </p></a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default header
